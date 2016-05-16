eb0e65ff0ad2e540f521ad8eb7455615106cc194
lotfar <lutfurkayes@yahoo.com>
2016-05-16 13:26:19 +0200

    Update PurchaseOrder.js

--- services/PurchaseOrder.js
+++ services/PurchaseOrder.js
@@ -7,24 +7,4 @@ export default class PurchaseOrder extends CRUDService {
         this.endpoint = 'purchase_orders';
     }
 
-    purchaseOrders(){
-        return this.$http.get(this.apiURL + this.endpoint + '/'  ).then((purchase_orders) => purchase_orders.data);
-    }
-
-    purchaseOrder(id){
-        return this.$http.get(this.apiURL + this.endpoint + '/' + id).then((purchase_order) => purchase_order.data);
-    }
-
-    add(data){
-        return this.$http.post(this.apiURL + this.endpoint + '/' + data).then((purchase_order) => purchase_order.data);
-    }
-    
-    remove(id){
-        return this.$http.delete(this.apiURL + this.endpoint + '/' + id).then((purchase_order) => purchase_order.data);
-    }
-
-    edit(id, data){
-        return this.$http.put(this.apiURL + this.endpoint + '/' + id, data).then((purchase_order) => purchase_order.data);
-    }
-
 }
