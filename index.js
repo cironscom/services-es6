a11b34869ed98cebfac337f9afe9e194c47296c0
lotfar <lutfurkayes@yahoo.com>
2016-05-16 12:46:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -28,101 +28,20 @@ import Warehouse from './services/Warehouse';
 
 export default angular.module('app.services', [])
     .service('Authenticate', Authenticate)
-    .name;
-
-export default angular.module('app.services', [])
-    .service('Authentication', Authentication)
-    .name;
-
-export default angular.module('app.services', [])
-    .service('Calendar', Calendar)
-    .name;
-
-export default angular.module('app.services', [])
-    .service('Comment', Comment)
-    .name;
-
-export default angular.module('app.services', [])
-    .service('Contact', Contact)
-    .name;
-
-export default angular.module('app.services', [])
-    .service('crud', crud)
-    .name;
-
-export default angular.module('app.services', [])
-    .service('Dashboard', Dashboard)
-    .name;
-
-export default angular.module('app.services', [])
-    .service('Info', Info)
-    .name;
-
-export default angular.module('app.services', [])
-    .service('Interceptor', Interceptor)
-    .name;
-
-export default angular.module('app.services', [])
-    .service('Invoice', Invoice)
-    .name;
-
-export default angular.module('app.services', [])
-    .service('Notification', Notification)
-    .name;
-
-export default angular.module('app.services', [])
-    .service('Order', Order)
-    .name;
-
-export default angular.module('app.services', [])
-    .service('Payment', Payment)
-    .name;
-
-export default angular.module('app.services', [])
-    .service('Product', Product)
-    .name;
-
-export default angular.module('app.services', [])
-    .service('PurchaseOrder', PurchaseOrder)
-    .name;
-
-export default angular.module('app.services', [])
-    .service('Receipt', Receipt)
-    .name;
-
-export default angular.module('app.services', [])
-    .service('Service', Service)
-    .name;
-
-export default angular.module('app.services', [])
-    .service('stock', stock)
-    .name;
-
-export default angular.module('app.services', [])
-    .service('Supplier', Supplier)
-    .name;
-
-export default angular.module('app.services', [])
-    .service('SupplierInvoice', SupplierInvoice)
-    .name;
-
-export default angular.module('app.services', [])
-    .service('User', User)
-    .name;
-
-export default angular.module('app.services', [])
-    .service('UserSetting', UserSetting)
-    .name;
-
-export default angular.module('app.services', [])
-    .service('Vat', Vat)
-    .name;
-
-export default angular.module('app.services', [])
-    .service('Verification', Verification)
-    .name;
-
-export default angular.module('app.services', [])
-    .service('Warehouse', Warehouse)
+     .service('Authenticate', Authenticate)
+      .service('Authenticate', Authenticate)
+       .service('Authenticate', Authenticate)
+        .service('Authenticate', Authenticate)
+         .service('Authenticate', Authenticate)
+          .service('Authenticate', Authenticate)
+           .service('Authenticate', Authenticate)
+            .service('Authenticate', Authenticate)
+             .service('Authenticate', Authenticate)
+              .service('Authenticate', Authenticate)
+               .service('Authenticate', Authenticate)
+    
+    
+    
+    
     .name;
 
