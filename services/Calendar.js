3454506399ccf0c1a23d92dd9a51df5526910d1c
lotfar <lutfurkayes@yahoo.com>
2016-05-16 13:11:37 +0200

    Update Calendar.js

--- services/Calendar.js
+++ services/Calendar.js
@@ -10,20 +10,6 @@ export default class Calendar extends CRUDService {
     event(start, end){
     return this.$http.get(this.apiURL + this.endpoint + '/' start + '/' + end).then((events) => events.data);
     }
-
-    edit(id, data){
-    return this.$http.put(this.apiURL +this.endpoint + '/' + id, data).then((event) => events.data);
-    }
-
-
-    delete(id){
-    return this.$http.delete(this.apiURL + this.endpoint + '/' + id).then((status) => status.data);
-    }
-
-    add(data){
-    return this.$http.post(this.apiURL + this.endpoint + '/' + data).then((event) => status.data);
-
-    }
 }
 
             
