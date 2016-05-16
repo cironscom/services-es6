c159c34808c705ba2d2dc6a314355933a43d7c9e
lotfar <lutfurkayes@yahoo.com>
2016-05-16 13:33:44 +0200

    Update stock.js

--- services/stock.js
+++ services/stock.js
@@ -6,18 +6,5 @@ export default class stock extends CRUDService {
         super($http);
         this.endpoint = 'stocks';
     }
-
-    
-    warehouse(warehouse_id){
-        return this.$http.get(this.apiURL + this.endpoint + '/'  + warehouse_id + '/stock/').then((stock) => stock.data);
-    }
-
-    update(id, data){
-        return this.$http.put(this.apiURL + this.endpoint + '/'  + id, data).then((stock) => stock.data);
-    }
-    
-    add(warehouse_id, data){
-        return this.$http.post(this.apiURL + this.endpoint + '/'  + warehouse_id + '/stock/', data).then((stock) => stock.data);
-    }  
        
 }
