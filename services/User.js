05e484a13149d0f0abd148668dcf8bb4415d69a7
lotfar <lutfurkayes@yahoo.com>
2016-05-16 13:27:31 +0200

    Update User.js

--- services/User.js
+++ services/User.js
@@ -7,29 +7,4 @@ export default class User extends CRUDService {
         this.endpoint = 'users';
     }
 
-    
-    users(){
-        return this.$http.get(this.apiURL + this.endpoint + '/'  ).then((users) => users.data);
-    }
-     
-    user(id){
-        return this.$http.get(this.apiURL + this.endpoint + '/'  + id).then((user) => user.data);
-    }
-    
-    add(data){
-        return this.$http.post(this.apiURL + this.endpoint + '/'  ).then((user) => user.data);
-    }
-
-    remove(id){
-        return this.$http.delete(this.apiURL + this.endpoint + '/'  + id).then((user) => user.data);
-    }
-
-    edit(id, data){
-        return this.$http.put(this.apiURL + this.endpoint + '/'  + id, data).then((user) => user.data);
-    }
-      
-    changePassword(id, data){
-        return this.$http.put(this.apiURL + this.endpoint + '/'  + id, data + '/password').then((user) => user.data);
-    }
-
  }
