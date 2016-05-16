a1f9c95bfb959661a13f15a29bcd3d0d92b89686
lotfar <lutfurkayes@yahoo.com>
2016-05-16 13:24:18 +0200

    Update OrderRow.js

--- services/OrderRow.js
+++ services/OrderRow.js
@@ -6,25 +6,4 @@ export default class OrderRow extends CRUDService {
         super($http);
         this.endpoint = 'order_rows';
     }
-
-     rows(){
-        return this.$http.get(this.apiURL + this.endpoint + '/' ).then((orderrows) => orderrows.data);
-    }
-
-     row(id){
-        return this.$http.get(this.apiURL + this.endpoint + '/' + id).then((orderrow) => orderrow.data);
-    }
-
-     add(data){
-        return this.$http.post(this.apiURL + this.endpoint + '/' + data).then((orderrow) => orderrow.data);
-    }
-
-     remove(id){
-        return this.$http.delete(this.apiURL + this.endpoint + '/' + id).then((orderrow) => orderrow.data);
-    }
-
-    edit(id, data){
-        return this.$http.put(this.apiURL + this.endpoint + '/' + id, data).then((orderrow) => orderrow.data);
-    }
-
 }
