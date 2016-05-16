a62b1b5c127e1c6015d0f0ce1e8bbb9d31e98b03
lotfar <lutfurkayes@yahoo.com>
2016-05-16 13:12:42 +0200

    Update Comment.js

--- services/Comment.js
+++ services/Comment.js
@@ -11,21 +11,4 @@ export default class Comment extends CRUDService {
   comments(url){
         return this.$http.get(this.apiURL + this.endpoint + '/' + url).then((comments) => comments.data);
     }
-
-  comments(id){
-        return this.$http.get(this.apiURL + this.endpoint + '/' + id).then((comment) => comment.data);
-    }
- 
-  add(data){
-        return this.$http.post(this.apiURL + this.endpoint + '/' + data).then((comment) => comment.data);
-    }
-
-  remove(id){
-        return this.$http.post(this.apiURL + this.endpoint + '/' + id).then((comment) => comment.data);
-    }
-
-  edit(id, data){
-        return this.$http.post(this.apiURL + this.endpoint + '/' +id, data).then((comment) => comment.data);
-    }
-
 }
