f81a5005467c19ce90a953e4431f90b4f02c0bd1
lotfar <lutfurkayes@yahoo.com>
2016-05-16 13:28:41 +0200

    Update Warehouse.js

--- services/Warehouse.js
+++ services/Warehouse.js
@@ -7,29 +7,4 @@ export default class Warehouse extends CRUDService {
         this.endpoint = 'warehouses';
     }
 
-    
-    warehouses(){
-        return this.$http.get(this.apiURL + this.endpoint + '/'  ).then((warehouses) => warehouses.data);
-    }
-
-    count(){
-        return this.$http.get(this.apiURL + this.endpoint + '/'  ).then((warehouses) => warehouses.data);
-    }
-
-    warehouses(id){
-        return this.$http.get(this.apiURL + this.endpoint + '/'  + id).then((warehouse) => warehouse.data);
-    }
-
-    add(data){
-        return this.$http.post(this.apiURL + this.endpoint + '/'  + data).then((warehouse) => warehouse.data);
-    }
-
-    remove(id){
-        return this.$http.delete(this.apiURL + this.endpoint + '/'  + id).then((warehouse) => warehouse.data);
-    }
-
-     edit(id, data){
-        return this.$http.put(this.apiURL + this.endpoint + '/'  + id, data).then((warehouse) => warehouse.data);
-    }
-
 }
