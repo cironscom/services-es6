3137457e5dba6041c6ad2160c256f5bb2953d82f
lotfar <lutfurkayes@yahoo.com>
2016-05-16 13:26:43 +0200

    Update Receipt.js

--- services/Receipt.js
+++ services/Receipt.js
@@ -7,28 +7,4 @@ export default class Receipt extends CRUDService {
         this.endpoint = 'receipts';
     }
 
-    receipts(){
-        return this.$http.get(this.apiURL + this.endpoint + '/' ).then((receipts) => receipts.data);
-    }
-
-    count(){
-        return this.$http.get(this.apiURL + this.endpoint + '/' ).then((receipts) => receipts.data);
-    }
-
-    receipt(id){
-        return this.$http.get(this.apiURL + this.endpoint + '/' + id).then((receipt) => receipt.data);
-    }
-
-    add(data){
-        return this.$http.post(this.apiURL + this.endpoint + '/' + data).then((receipt) => receipt.data);
-    }
-  
-    remove(id){
-        return this.$http.delete(this.apiURL + this.endpoint + '/' + id).then((receipt) => receipt.data);
-    }
-
-    edit(id, data){
-        return this.$http.put(this.apiURL + this.endpoint + '/' + id, data).then((receipt) => receipt.data);
-    }
-
 }
