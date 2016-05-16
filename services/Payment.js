8d7e111af189956a06bec775d0b4f7a8155b3114
lotfar <lutfurkayes@yahoo.com>
2016-05-16 13:24:51 +0200

    Update Payment.js

--- services/Payment.js
+++ services/Payment.js
@@ -7,24 +7,4 @@ export default class Payment extends CRUDService {
         this.endpoint = 'payments';
     }
 
-    payments(){
-        return this.$http.get(this.apiURL + this.endpoint + '/'  ).then((payments) => payments.data);
-    }
-
-    payment(id){
-        return this.$http.get(this.apiURL + this.endpoint + '/'  + id).then((payment) => payment.data);
-    }
-
-    add(data){
-        return this.$http.post(this.apiURL + this.endpoint + '/'  + data).then((payment) => payment.data);
-    }
-
-    remove(id){
-        return this.$http.delete(this.apiURL + this.endpoint + '/'  + id).then((payment) => payment.data);
-    }
-
-    payment(id, data){
-        return this.$http.put(this.apiURL + this.endpoint + '/'  + id, data).then((payment) => payment.data);
-    }
-
 }
