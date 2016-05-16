7994d62086b546a1d9b6887bac927f48007791f4
lotfar <lutfurkayes@yahoo.com>
2016-05-16 13:36:11 +0200

    Update SupplierInvoice.js

--- services/SupplierInvoice.js
+++ services/SupplierInvoice.js
@@ -7,25 +7,4 @@ export default class SupplierInvoice extends CRUDService {
         this.endpoint = 'supplier_invoices';
     }
 
-   
-    supplierInvoices(){
-        return this.$http.get(this.apiURL + this.endpoint + '/'  ).then((supplier_invoices) => supplier_invoices.data);
-    }
-
-     supplierInvoice(id){
-        return this.$http.get(this.apiURL + this.endpoint + '/'  + id).then((supplier_invoice) => supplier_invoice.data);
-    }
-
-     add(data){
-        return this.$http.post(this.apiURL + this.endpoint + '/'  ).then((supplier_invoice) => supplier_invoice.data);
-    }
-
-     remove(id){
-        return this.$http.delete(this.apiURL + this.endpoint + '/'  + id).then((supplier_invoice) => supplier_invoice.data);
-    }
-
-     eidt(id, data){
-        return this.$http.put(this.apiURL + this.endpoint + '/'  + id, data).then((supplier_invoice) => supplier_invoice.data);
-    }
-
 }
