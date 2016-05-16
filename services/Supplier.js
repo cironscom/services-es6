ec7fe6aff474c8286466151863c01117477de08c
lotfar <lutfurkayes@yahoo.com>
2016-05-16 13:27:02 +0200

    Update Supplier.js

--- services/Supplier.js
+++ services/Supplier.js
@@ -7,25 +7,4 @@ export default class Supplier extends CRUDService {
         this.endpoint = 'suppliers';
     }
 
-    
-    suppliers(){
-        return this.$http.get(this.apiURL + this.endpoint + '/'  ).then((suppliers) => suppliers.data);
-    }
-
-    supplier(id){
-        return this.$http.get(this.apiURL + this.endpoint + '/'  + id).then((supplier) => supplier.data);
-    }
-    
-    add(data){
-        return this.$http.post(this.apiURL + this.endpoint + '/'  + data).then((supplier) => supplier.data);
-    }
-
-    remove(id){
-        return this.$http.delete(this.apiURL + this.endpoint + '/'  + id).then((supplier) => supplier.data);
-    }
-
-    edit(id, data){
-        return this.$http.put(this.apiURL + this.endpoint + '/'  + id, data).then((supplier) => supplier.data);
-    }
-
  }
