b1ad3140c500ccefbccaa4d1bf6bd78be6de3a97
lotfar <lutfurkayes@yahoo.com>
2016-05-16 13:42:01 +0200

    Update Contact.js

--- services/Contact.js
+++ services/Contact.js
@@ -11,8 +11,4 @@ export default class Contact extends CRUDService {
         return this.$http.get(this.apiURL + this.endpoint + '/'  ).then((contacts) => contacts.data);
     }
 
-    remove(id){
-        return this.$http.delete(this.apiURL + this.endpoint + '/' + id).then((contact) => contact.data);
-    }
-
 }
