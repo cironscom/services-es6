90dc52e482e9f4090522ae0cdbe637f7091cd714
lotfar <lutfurkayes@yahoo.com>
2016-05-16 13:18:11 +0200

    Update Invoice.js

--- services/Invoice.js
+++ services/Invoice.js
@@ -7,11 +7,6 @@ export default class Invoice extends CRUDService {
         this.endpoint = 'invoices';
     }
 
-    // ----- add all extra methods except the CRUD.
- 	invoices(){
-        return this.$http.get(this.apiURL + this.endpoint + '/'  ).then((invoices) => invoices.data);
-    }
-
     count(){
         return this.$http.get(this.apiURL + this.endpoint + '/'  ).then((invoices) => invoices.data);
     }
@@ -20,31 +15,15 @@ export default class Invoice extends CRUDService {
         return this.$http.get(this.apiURL + this.endpoint + '/'  ).then((invoices) => invoices.data);
     }
 
-	unpaidSum(){
+    unpaidSum(){
         return this.$http.get(this.apiURL + this.endpoint + '/'  ).then((invoices) => invoices.data);
-    }
-
-	invoice(id){
-        return this.$http.get(this.apiURL + this.endpoint + '/' + id).then((invoice) => invoice.data);
-    }
-
-	add(data){
-        return this.$http.post(this.apiURL + this.endpoint + '/' + data).then((invoice) => invoice.data);
-    }
-
-	remove(id){
-        return this.$http.delete(this.apiURL + this.endpoint + '/' + id).then((invoice) => invoice.data);
-    }
-
-	edit(id, data){
-        return this.$http.put(this.apiURL + this.endpoint + '/' + id, data).then((invoice) => invoice.data);
     }
 
     book(id){
         return this.$http.put(this.apiURL + this.endpoint + '/' + id + '/book').then((invoice) => invoice.data);
     }
 
-    credit(id){
+    makeCredit(id){
         return this.$http.post(this.apiURL + this.endpoint + '/' + id + '/credit').then((invoice) => invoice.data);
     }
 
