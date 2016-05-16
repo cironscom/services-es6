7b9cc71ca81bc246356281709d0863e890f5abfe
lotfar <lutfurkayes@yahoo.com>
2016-05-16 13:28:21 +0200

    Update Verification.js

--- services/Verification.js
+++ services/Verification.js
@@ -7,25 +7,4 @@ export default class Verification extends CRUDService {
         this.endpoint = 'verifications';
     }
 
-    
-    verifications(){
-        return this.$http.get(this.apiURL + this.endpoint + '/'  ).then((verifications) => verifications.data);
-    }
-
-    verification(id){
-        return this.$http.get(this.apiURL + this.endpoint + '/'  + id).then((verification) => verification.data);
-    }
-
-    add(data){
-        return this.$http.post(this.apiURL + this.endpoint + '/'  + data).then((verification) => verification.data);
-    }
-
-    remove(id){
-        return this.$http.delete(this.apiURL + this.endpoint + '/'  + id).then((verification) => verification.data);
-    }
-
-     edit(id, data){
-        return this.$http.put(this.apiURL + this.endpoint + '/'  + id, data).then((verification) => verification.data);
-    }
-
 }
